273055487b8934cf712508a3fcb2cd6d2d839923
Nurik <nurzhigit.turman@narxoz.kz>
2024-11-19 02:27:32 +0500

    add pages

--- src/Components/WelcomePage/index.jsx
+++ src/Components/WelcomePage/index.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import "./styles/style.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles/style.css";
 const WelcomePage = () => {
   return (
-    <div className={'welcomePage'}>
-      <div className={'Darkness'}></div>
-      <h1 className={'welcomePage_h1'}>Добро пожаловать!</h1>
-      <Link to="/login"><button className={'welcomePage_button'}>Войти</button></Link>
-      <p className={'welcomePage_p'}>Нет аккаунта? <Link to="/register">Зарегистрироваться</Link></p>
+    <div className={"welcomePage"}>
+      <div className={"Darkness"}></div>
+      <h1 className={"welcomePage_h1"}>Добро пожаловать!</h1>
+      <Link to="/login">
+        <button className={"welcomePage_button"}>Войти</button>
+      </Link>
+      <p className={"welcomePage_p"}>
+        Нет аккаунта? <Link to="/register">Зарегистрироваться</Link>
+      </p>
     </div>
   );
 };
